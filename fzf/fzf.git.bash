c6ab0e2868f9abdf1b9a73dcb688e7b01729a943
Jim Zin <jzin@apple.com>
2017-12-20 14:19:44 -0800

    Add fzf

---
+++ fzf/fzf.git.bash
@@ -0,0 +1,16 @@
+gco() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout "$(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")"
+}
+
+gre() {
+  local branches branch
+  branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git rebase "$(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")" "$@"
+}
+
